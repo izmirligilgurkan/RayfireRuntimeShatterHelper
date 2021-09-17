1c19af44a15bb94fae449175159ffe7a060e3064
izmirligilgurkan <izmirligilgurkan@gmail.com>
2021-09-18 02:22:57 +0300

    v1.0
    Script and Readme added.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # RayfireRuntimeShatterHelper
 Attach and go script to just shatter objects using RayFire for Unity library.
+
+You need RayFire for Unity in your project!
+
+Instructions:
+    1. Attach the script to any GameObject.
+    2. Set your mesh and materials if your object didn't have them.
+    3. Set the editor settings to your liking.
+    4. Call the ShatterObject public method from script.
---
+++ ShatterRuntime.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using RayFire;
+using UnityEngine;
+
+[SelectionBase, RequireComponent(typeof(MeshCollider)),
+ RequireComponent(typeof(MeshRenderer)), RequireComponent(typeof(MeshFilter))]
+public class ShatterRuntime : MonoBehaviour
+    {
+        private GameObject fragmentRoot;
+        private MeshRenderer meshRenderer;
+        private MeshCollider meshCollider;
+        private RayfireRigid rayfireRigid;
+        private RayfireShatter rayfireShatter;
+        private bool shattered;
+        
+        
+        [Header("Editor Settings")]
+        public MaterialType fragmentType;
+        public int fragmentCount = 10;
+        public int fragmentLayerIndex;
+        public float fragmentLife = 3f;
+        public bool fragmentUseGravity = true;
+
+        [Space(5)] [Header("Optional Settings")]
+        public bool parentFragments;
+        public Transform parentForFragments;
+        public bool changeInnerMaterial;
+        public Material innerMaterial;
+        public bool useParticleAfterShatter;
+        public ParticleSystem shatterParticlePrefab;
+        public Vector3 particleForward = Vector3.up;
+        
+
+        private IEnumerator Start()
+        {
+            if (!gameObject.activeSelf) yield break;
+            meshRenderer = GetComponent<MeshRenderer>();
+            meshCollider = GetComponent<MeshCollider>();
+            rayfireShatter = gameObject.AddComponent<RayfireShatter>();
+            rayfireShatter.voronoi.amount = fragmentCount;
+            rayfireShatter.mode = FragmentMode.Runtime;
+            if (changeInnerMaterial)
+            {
+                rayfireShatter.material.innerMaterial = innerMaterial;
+            }
+            rayfireShatter.Fragment();
+            rayfireShatter.centerPosition = transform.position + Vector3.up;
+            rayfireShatter.voronoi.centerBias = 1;
+            fragmentRoot = rayfireShatter.rootChildList[0].gameObject;
+            fragmentRoot.transform.SetParent(transform);
+            rayfireRigid = fragmentRoot.AddComponent<RayfireRigid>();
+            rayfireRigid.activation.layer = "Fragments";
+            rayfireRigid.physics.materialType = fragmentType;
+            rayfireRigid.physics.useGravity = fragmentUseGravity; 
+            rayfireRigid.fading.fadeType = FadeType.Destroy;
+            rayfireRigid.fading.lifeTime = fragmentLife;
+            rayfireRigid.fading.lifeVariation = .5f;
+            rayfireRigid.fading.onActivation = true;
+            rayfireRigid.fading.fadeTime = .1f;
+            rayfireRigid.objectType = ObjectType.MeshRoot;
+            fragmentRoot.SetActive(false);
+        }
+        public void ShatterObject(float forceMagnitude = 1f, Vector3 forceDirection = default, bool explode = false)
+        {
+            if (!gameObject.activeSelf || shattered) return;
+            if (useParticleAfterShatter)
+            {
+                var shatterParticleInstance = Instantiate(shatterParticlePrefab);
+                var transform1 = shatterParticleInstance.transform;
+                transform1.position = transform.position;
+                transform1.forward = particleForward;
+            }
+            shattered = true;
+            fragmentRoot.SetActive(true);
+            meshRenderer.enabled = false;
+            meshCollider.enabled = false;
+            rayfireRigid.Initialize();
+            foreach (var fragment in rayfireShatter.fragmentsAll)
+            {
+                var direction = explode
+                    ? (fragment.transform.position - meshCollider.bounds.center).normalized
+                    : forceDirection.normalized;
+                forceMagnitude = Mathf.Clamp(forceMagnitude, 0f, 100f);
+                fragment.transform.SetParent(parentFragments ? parentForFragments : null, true);
+                fragment.layer = fragmentLayerIndex;
+                fragment.GetComponent<Rigidbody>().AddForce(direction * forceMagnitude * 3f, ForceMode.VelocityChange);
+            }
+            rayfireRigid.Fade();
+        }
+    }
